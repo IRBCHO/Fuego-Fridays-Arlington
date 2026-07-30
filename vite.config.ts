52211166459110204b33f48fe051c45cf643a0d5
bysnyman <bysnyman@gmail.com>
2026-07-30 12:03:53 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
-  base: “/Fuego-Fridays-Arlington/“,
+  base: '/Fuego-Fridays-Arlington/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
